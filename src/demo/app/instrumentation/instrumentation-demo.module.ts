2a6331b436e5b0f6b847852483c3a2d431243e8a
Ryan Blace <reblace@gmail.com>
2017-12-29 00:23:00 -0500

    Export factory method for AOT build

--- src/demo/app/instrumentation/instrumentation-demo.module.ts
+++ src/demo/app/instrumentation/instrumentation-demo.module.ts
@@ -45,6 +45,6 @@ import { DefaultComponent } from './default.component';
 })
 export class InstrumentationDemoModule { }
 
-function serverInstrumentationServiceFactory(httpBackend: HttpBackend) {
+export function serverInstrumentationServiceFactory(httpBackend: HttpBackend) {
 	return new ServerInstrumentationService(httpBackend, '/api/metrics');
 }
